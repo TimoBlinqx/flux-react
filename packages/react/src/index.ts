f5515e597afb96903a6623ea2f32bd3c27c2634b
Timo Roos <t.roos@blisdigital.com>
2026-09-11 22:20:50 +0200

    feat(react): port menu component family

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -9,6 +9,7 @@ export * from './components/Feedback';
 export * from './components/Forms';
 export * from './components/Icon';
 export * from './components/Layout';
+export * from './components/Menus';
 export * from './components/Navigation';
 export * from './components/Notifications';
 export * from './components/Overlays';
